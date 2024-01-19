bf66b858cc9d1ccc8d6be611a7e9ff97198a4229
Kasshabz <142917268+Kasshabz@users.noreply.github.com>
2024-01-19 17:07:16 +0000

    task 10

--- models/users-models.js
+++ models/users-models.js
@@ -2,7 +2,6 @@ const db = require("../db/connection");
 const fetchUsers = () => {
   let queryStr = "SELECT * FROM users";
   return db.query(queryStr).then(({ rows }) => {
-    console.log(rows);
     return rows;
   });
 };
